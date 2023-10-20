47eff866f75b4e516367ce126d13f07e3423028e
Artem199627 <artemishko@gmail.com>
2023-10-20 20:41:37 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+
+import { galleryItems } from "./gallery-items";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const gallery = document.querySelector(".gallery");
+
+
+
+
+const markup = galleryItems.map(({ preview, description, original }) => `<li class="gallery__item">
+      <a class="gallery__link" href=${original}>
+        <img
+          class="gallery__image"
+          src=${preview}
+          alt=${description}
+        />
+      </a>
+    </li>`).join("");
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+const lightbox = new SimpleLightbox(".gallery__item a", {
+    captionsData: "alt",
+    captionDelay : 250,     
+});
