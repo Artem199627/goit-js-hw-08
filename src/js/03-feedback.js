71160ca87016853e3a75f2ed1d6746cbe91deb48
Artem199627 <artemishko@gmail.com>
2023-10-27 20:53:23 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
 import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+
+form.addEventListener("submit", onFormSubmit);
+form.addEventListener("input", throttle(onFormInput, 500));
+
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+};
+
+
+function onFormInput() {
+    let messageData = {
+        email: form.elements.email.value,
+        message: form.elements.message.value,
+    };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(messageData));
+    return messageData;
+}
+
+function inputField() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    if (savedMessage) {
+        form.elements.email.value = JSON.parse(savedMessage).email;
+        form.elements.message.value = JSON.parse(savedMessage).message;
+    }
+}
+
+inputField();
